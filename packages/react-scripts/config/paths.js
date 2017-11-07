74609a4f90cce1e3662a4ebdb37fe46aaa59e790
Sibyl <sibyl@wormbase.org>
2017-11-07 19:16:30 +0000

    change appSrcLegacy to root instead of root/js

--- packages/react-scripts/config/paths.js
+++ packages/react-scripts/config/paths.js
@@ -80,7 +80,7 @@ module.exports = {
   appIndexJs: resolveApp('src/index.js'),
   appPackageJson: resolveApp('package.json'),
   appSrc: resolveApp('src'),
-  appSrcLegacy: resolveApp('../root/js'),
+  appSrcLegacy: resolveApp('../root'),
   yarnLockFile: resolveApp('yarn.lock'),
   testsSetup: resolveApp('src/setupTests.js'),
   appNodeModules: resolveApp('node_modules'),
