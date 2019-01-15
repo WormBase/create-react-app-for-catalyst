45dd5a5260047774021a07fb5f4a183ea1e68f27
Sibyl <sibyl@wormbase.org>
2019-01-15 01:25:24 +0000

    fix absolute path in bundle messing up hash

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -292,7 +292,7 @@ module.exports = {
                   options: {
                     importLoaders: 1,
                     minimize: false,
-                    sourceMap: true,
+                    sourceMap: false,
                   },
                 },
                 {
