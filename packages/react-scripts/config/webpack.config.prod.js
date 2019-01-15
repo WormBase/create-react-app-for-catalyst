217b99bdc5a83a624ba3abfd850a6f814f8774b4
Sibyl <sibyl@wormbase.org>
2019-01-15 01:27:43 +0000

    Revert "no minifying of css"
    This reverts commit 906344a9e1bffd9cf01014ac2a5ed1bb47827d9d.

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -248,7 +248,7 @@ module.exports = {
                   loader: require.resolve('css-loader'),
                   options: {
                     importLoaders: 1,
-                    minimize: false,
+                    minimize: true,
                     sourceMap: true,
                     root: '..',
                   },
@@ -291,7 +291,7 @@ module.exports = {
                   loader: require.resolve('css-loader'),
                   options: {
                     importLoaders: 1,
-                    minimize: false,
+                    minimize: true,
                     sourceMap: false,
                   },
                 },
